58aff66b1b7d3f03793889082152c86e3370cdd9
Tobias Heilig <the@objektkultur.de>
2020-01-29 09:19:53 +0100

    Add entity logical name attributes

---
+++ Xrm.WebApi/EntityLogicalCollectionNameAttribute.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Copyright (c) 2020 Tobias Heilig
+ * 
+ * BSD 3-Clause
+ * see LICENCE file for details.
+ */
+
+using System;
+
+namespace Xrm.WebApi
+{
+    class EntityLogicalCollectionNameAttribute : Attribute
+    {
+    }
+}
