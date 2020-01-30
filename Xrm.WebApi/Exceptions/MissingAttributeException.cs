64a35e876f4f4ba13eb8db5120845b5e25d0ef73
Tobias Heilig <the@objektkultur.de>
2020-01-30 08:10:51 +0100

    Change protection level of MissingAttributeException to public

--- Xrm.WebApi/Exceptions/MissingAttributeException.cs
+++ Xrm.WebApi/Exceptions/MissingAttributeException.cs
@@ -9,7 +9,7 @@ using System;
 
 namespace Xrm.WebApi.Exceptions
 {
-    internal class MissingAttributeException : Exception
+    public class MissingAttributeException : Exception
     {
         public MissingAttributeException(string type, string attribute) :
             base($"{type} is missing expected attribute {attribute}")
