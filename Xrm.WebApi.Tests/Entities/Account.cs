b272e32da4dab23626ccc82289fe7986b16d3ead
Tobias Heilig <the@objektkultur.de>
2020-01-30 08:04:06 +0100

    Add account entity without entity logical name attributes to test missing attribute scenarios

---
+++ Xrm.WebApi.Tests/Entities/Account.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Copyright (c) 2020 Tobias Heilig
+ * 
+ * BSD 3-Clause
+ * see LICENCE file for details.
+ */
+
+using System.Text.Json.Serialization;
+
+namespace Xrm.WebApi.Tests.Entities
+{
+    /// <summary>
+    /// Json model for the Dynamics 365 Xrm account entity.
+    /// Missing <see cref="EntityLogicalCollectionNameAttribute"/>
+    /// and <see cref="EntityLogicalNameAttribute"/>.
+    /// </summary>
+    internal class Account
+    {
+        /// <summary>
+        /// The guid of the account.
+        /// </summary>
+        [JsonPropertyName("Accountid")]
+        public string? AccountId { get; set; }
+    }
+}
