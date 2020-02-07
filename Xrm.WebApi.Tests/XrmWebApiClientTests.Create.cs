17c719d50533ce102f8bc4a9425039e27fd0b660
Tobias Heilig <the@objektkultur.de>
2020-02-07 19:52:50 +0100

    Add test case for CreateAsync

---
+++ Xrm.WebApi.Tests/XrmWebApiClientTests.Create.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Copyright (c) 2020 Tobias Heilig
+ * 
+ * BSD 3-Clause
+ * see LICENCE file for details.
+ */
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using System;
+using System.Threading.Tasks;
+
+using Xrm.WebApi.Tests.Entities;
+
+namespace Xrm.WebApi.Tests
+{
+    public partial class XrmWebApiClientTests
+    {
+        [TestMethod]
+        [TestCategory("Create")]
+        [TestCategory("Positive")]
+        public async Task CreateAsync_WithEmptyRecord_ShouldReturnResultGuid()
+        {
+            var record = new Contact();
+            var id = await _xrmWebApiClient.CreateAsync<Contact>(record);
+
+            Assert.IsNotNull(id);
+            Assert.IsInstanceOfType(id, typeof(Guid));
+        }
+    }
+}
